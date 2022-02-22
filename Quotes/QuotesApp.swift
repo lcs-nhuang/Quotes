6fb4c7b540a2857bb835fc23d3499f3aa30b27bf
Nicole Huang <nhuang@lcs.on.ca>
2022-02-22 12:19:09 -0500

    Initial Commit

---
+++ Quotes/QuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuotesApp.swift
+//  Quotes
+//
+//  Created by Nicole on 2022/2/22.
+//
+
+import SwiftUI
+
+@main
+struct QuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
