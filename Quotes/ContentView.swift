6dff72d71bdc656494c6611b4efc1d3f277e78ab
Nicole Huang <nhuang@lcs.on.ca>
2022-02-22 16:53:46 -0500

    Finished the static layout of the app.

--- Quotes/ContentView.swift
+++ Quotes/ContentView.swift
@@ -9,13 +9,55 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            Text("I am a quote")
+                .font(.title)
+                .multilineTextAlignment(.leading)
+                .padding()
+                .border(Color.black,width: 4)
+                .padding(10)
+                .padding()
+            
+            Image(systemName: "heart.circle")
+                .resizable()
+                .frame(width: 40, height: 40)
+                .foregroundColor(Color.red)
+                
+            
+            Button(
+                action: {
+                    print("I've been pressed.")
+                },
+               
+                label:{
+                    Text("Another one!")
+                }
+            )
+                .buttonStyle(.bordered)
+                .padding()
+                
+            
+            HStack{
+                Text("Favourites")
+                    .bold()
+                Spacer()
+            }
+            
+            List {
+                Text("Nothing is a waste of time if you use the experience wisely.")
+                Text("If your actions inspire others to dream more, learn more, do more and become more, you are a leader.")
+            }
+            Spacer()
+        }
+        .navigationTitle("Quotes")
+        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
+        NavigationView{
         ContentView()
+        }
     }
 }
